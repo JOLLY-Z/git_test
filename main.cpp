f8117ee59e1ee9bf2dc5206b6fe441fefc9b45a1
Jolly_PC <18719197319@163.com>
2025-07-02 13:33:42 +0800

    创建main.cpp文件，写了初始的代码，未完成sort函数

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void sort(int* arr, int size)
+{
+	
+}
+
+int main()
+{
+	int arr[] = {12, 10, 65, 74, 34, 2, 8, 26, 31, 17};
+
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	sort(arr, size);
+
+	return 0;
+}
